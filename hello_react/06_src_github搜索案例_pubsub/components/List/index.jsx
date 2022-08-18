fe2e5a543313e6dd88cd0e787c158fe6b2256aa7
Alex.Zhang <alex.zhang@take2games.com>
2022-08-18 20:56:11 +0800

    refactor: code beutify

--- hello_react/06_src_github搜索案例_pubsub/components/List/index.jsx
+++ hello_react/06_src_github搜索案例_pubsub/components/List/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import './index.css'
 import PubSub from 'pubsub-js'
 
@@ -7,42 +7,47 @@ class List extends Component {
         users: [],
         isFirst: true,
         isLoading: false,
-        err: ''
+        err: '',
     }
 
     componentDidMount() {
         PubSub.subscribe('search', (_, stateObj) => {
-            this.setState({...stateObj})
+            this.setState({ ...stateObj })
         })
     }
 
-
     render() {
-        const {users, isFirst, isLoading, err} = this.state
+        const { users, isFirst, isLoading, err } = this.state
         return (
             <div className="row">
-                {
-                    isFirst ? <h2>Enter GitHub Username</h2> :
-                        isLoading ? <h2>Loading....</h2> :
-                            err ? <h2>{err}</h2> :
-                                users.map((userObj) => {
-                                        return (
-                                            <div key={userObj.id} className="card">
-                                                <a rel="noreferrer" href={userObj.html_url} target="_blank">
-                                                    <img alt="avatar" src={userObj.avatar_url}
-                                                         style={{width: '100px'}}/>
-                                                </a>
-                                                <p className="card-text">{userObj.login}</p>
-                                            </div>
-                                        )
-                                    }
-                                )
-                }
+                {isFirst ? (
+                    <h2>Enter GitHub Username</h2>
+                ) : isLoading ? (
+                    <h2>Loading....</h2>
+                ) : err ? (
+                    <h2>{err}</h2>
+                ) : (
+                    users.map((userObj) => {
+                        return (
+                            <div key={userObj.id} className="card">
+                                <a
+                                    rel="noreferrer"
+                                    href={userObj.html_url}
+                                    target="_blank">
+                                    <img
+                                        alt="avatar"
+                                        src={userObj.avatar_url}
+                                        style={{ width: '100px' }}
+                                    />
+                                </a>
+                                <p className="card-text">{userObj.login}</p>
+                            </div>
+                        )
+                    })
+                )}
             </div>
-
-        );
+        )
     }
-
 }
 
-export default List;
+export default List
